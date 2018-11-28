c304c74eb0c91049f62b872195f27c0b17730095
League <league@WTS9.attlocal.net>
2018-11-27 18:27:51 -0800

    great day
    bannana and dizzy robot

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,15 +10,34 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+	String qaz=	JOptionPane.showInputDialog("do you like BANANAS");
 		//2. if they say no, 
+	if (qaz.equals("yes")) {
+		JOptionPane.showInputDialog("So whats your hobbies");
+	JOptionPane.showMessageDialog(null, "That is much better with a bannana");
+		}
+	else if (qaz.equals("no")) { JOptionPane.showMessageDialog(null, "WHY,BANNANAS ARE THE BEST");
+		
+		
+		
+	}
+	
+			
+			
 			//tell them they are crazy 
+			
+			
+			
+			
+			
+			
 			//and end quiz
 		//3. if they say yes
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
+		//4. OPTIONAL: if they say something other than “yes�? or “no�?
+		//	show a pop up that says “You are bananas!�?
 	
 	}
 
--- src/extra/DizzyRobot.java
+++ src/extra/DizzyRobot.java
@@ -3,17 +3,22 @@
 
 package extra;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
 	public static void main(String[] args) {
-		
+robot.setSpeed(59);
 		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-
+String qaz=JOptionPane.showInputDialog("how many times should the robot spin");
  // 1. Use the dance method to make the robot spin.
-	
 
+
+int wsx= Integer.parseInt(qaz);
+	
+dance(wsx);
 	}
 
 	static void dance(int numberOfSpins) {
