bade31e63e2d312b916e2b4a8af65e4a7df21293
League <league@WTS8.attlocal.net>
2018-12-18 18:27:29 -0800

    finnished walked of fame and part of fizzz buz

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -11,22 +11,34 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
-rob.setX(144);
+		rob.setAngle(90);
+		rob.setSpeed(200);
+		rob.setX(144);
 		// 2. Make the robot draw a star shape. Hint: 144.
-rob.penDown();
-	rob.move(100); rob.turn(144);; rob.move(100); rob.turn(144);; rob.move(100); rob.turn(144);; rob.move(100); rob.turn(144);; rob.move(100);
-rob.penUp(); rob.move(100);
+		
 		// 3. Set the size of the star to 30.
-rob.penDown();
-for (int i = 0; i < args.length; i++) {
-	
-}
+		rob.penDown();
+		for (int i = 0; i < 10; i++) {
+			rob.penDown(); rob.setRandomPenColor();
+			for (int i1 = 0; i1 < 10; i1++) {
+
+				rob.move(30);
+				rob.turn(144);
+
+			}
+			rob.setAngle(90);
+			rob.penUp();
+			
+			rob.move(50);
+
+		}
+		rob.move(1000);
+
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walkOfFame
+		 * Make the robot draw a line of stars like this: http://bit.ly/walkOfFame
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
